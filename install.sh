61b5fc0ac00c65c4ac63667baa429693c9dadf48
AXeL-dev <anass_denna@hotmail.fr>
2018-10-30 21:44:29 +0100

    fix install script

--- install.sh
+++ install.sh
@@ -12,21 +12,19 @@ cp -R icons /usr/share/silaty/
 cp -R audio /usr/share/silaty/
 cp -R data /usr/share/silaty/
 
-cp /usr/share/silaty/icons/hicolor/128x128/apps/silaty.svg /usr/share/icons/hicolor/scalable/apps/
-chmod 644  /usr/share/icons/hicolor/scalable/apps/silaty.svg
+cp icons/hicolor/128x128/apps/silaty.svg /usr/share/icons/hicolor/scalable/apps/
+chmod 644 /usr/share/icons/hicolor/scalable/apps/silaty.svg
 
-cp /usr/share/silaty/icons/hicolor/128x128/apps/silaty.svg /usr/share/icons/hicolor/128x128/apps/
-chmod 644  /usr/share/icons/hicolor/128x128/apps/silaty.svg
+cp icons/hicolor/128x128/apps/silaty.svg /usr/share/icons/hicolor/128x128/apps/
+chmod 644 /usr/share/icons/hicolor/128x128/apps/silaty.svg
 
-cp /usr/share/silaty/icons/hicolor/48x48/apps/silaty.svg /usr/share/icons/hicolor/48x48/apps/
-chmod 644  /usr/share/icons/hicolor/48x48/apps/silaty.svg
+cp icons/hicolor/48x48/apps/silaty.svg /usr/share/icons/hicolor/48x48/apps/
+chmod 644 /usr/share/icons/hicolor/48x48/apps/silaty.svg
 
-cp /usr/share/silaty/icons/hicolor/24x24/apps/silaty.svg /usr/share/icons/hicolor/24x24/apps/
-chmod 644  /usr/share/icons/hicolor/24x24/apps/silaty.svg
+cp icons/hicolor/24x24/apps/silaty.svg /usr/share/icons/hicolor/24x24/apps/
+chmod 644 /usr/share/icons/hicolor/24x24/apps/silaty.svg
 
 cp *.py /usr/share/silaty/
-#pycompile silaty-indicator
-mv /usr/share/silaty/silaty-indicator.py /usr/share/silaty/silaty-indicator
 chmod 755 -R /usr/share/silaty/
 
 cp silaty.desktop /etc/xdg/autostart/
@@ -35,7 +33,7 @@ chmod 755 /etc/xdg/autostart/silaty.desktop
 cp silaty.desktop /usr/share/applications/
 chmod 755 /usr/share/applications/silaty.desktop
 
-ln -s /usr/share/silaty/silaty-indicator /usr/local/bin/silaty-indicator
+ln -s /usr/share/silaty/silaty-indicator.py /usr/local/bin/silaty-indicator
 chmod 755 /usr/local/bin/silaty-indicator
 
 gtk-update-icon-cache
